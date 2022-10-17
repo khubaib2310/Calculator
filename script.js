3b2b1a71b73119ca0194e2ec1d14bd70710ac08b
Muhammad Khubaib <khubaibmuhammad964@gmail.com>
2022-10-17 14:55:37 +0500

    calculator change

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let string = "";
 let buttons = document.querySelectorAll(".button");
+
+const calculator = function (e) {
+  if (e.target.innerHTML == "=") {
+    string = eval(string);
+    document.querySelector("input").value = string;
+  } else if (e.target.innerHTML == "C") {
+    string = "";
+    document.querySelector("input").value = string;
+  } else {
+    string = string + e.target.innerHTML;
+    document.querySelector("input").value = string;
+    console.log(e.target);
+  }
+} 
+
+
 Array.from(buttons).forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      document.querySelector("input").value = string;
-    } else if (e.target.innerHTML == "C") {
-      string = "";
-      document.querySelector("input").value = string;
-    } else {
-      string = string + e.target.innerHTML;
-      document.querySelector("input").value = string;
-      console.log(e.target);
-    }
-  });
+  button.addEventListener("click", calculator
+  
+  );
 });
